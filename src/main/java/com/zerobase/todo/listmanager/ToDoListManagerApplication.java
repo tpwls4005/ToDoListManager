f8bce1dc5f0b0fd1245c02c43aef36089a338ee1
Min <tpwls4005@naver.com>
2023-10-24 23:30:22 +0900

    commit..

---
+++ src/main/java/com/zerobase/todo/listmanager/ToDoListManagerApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.todo.listmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zerobase/todo/listmanager/ToDoListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerobase.todo.listmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
